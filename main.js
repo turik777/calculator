2102be1ccc121e369090ecbd8ef6143bfba9a87b
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-11 00:58:14 +0300

    Rework calculator logic

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
 function addNumbers(a, b) {
     value = a + b;
     display.textContent = value;
+    firstNumber = value;
 }
 
 function subtractNumbers(a, b) {
     value = a - b;
     display.textContent = value;
+    firstNumber = value;
 }
 
 function multiplyNumbers(a, b) {
     value = a * b;
     display.textContent = value;
+    firstNumber = value;
 }
 
 function divideNumbers(a, b) {
     value = a / b;
     display.textContent = value;
+    firstNumber = value;
+    if (value === Infinity) display.textContent = "What the heck are you doing?!";
 }
 
 let firstNumber;
 let secondNumber;
 let operator;
 
-function operate() {
+function operate(firstNumber, secondNumber, operator) {
     secondNumber = value;
-    if (operator === "addNumbers") return addNumbers(firstNumber, secondNumber);
-    if (operator === "subtractNumbers") return subtractNumbers(firstNumber, secondNumber);
-    if (operator === "multiplyNumbers") return multiplyNumbers(firstNumber, secondNumber);
-    if (operator === "divideNumbers") return divideNumbers(firstNumber, secondNumber);
-}
+    operator(firstNumber, secondNumber);
+};
 
 const display = document.querySelector("#display");
 const zero = document.querySelector("#zero");
@@ -51,86 +53,163 @@ const clear = document.querySelector("#clear");
 let value;
 
 function displayZero() {
-    display.textContent += 0;
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (!display.textContent || display.textContent !== "0") display.textContent += 0;
     value = parseInt(display.textContent);
 }
 
 function displayOne() {
-    display.textContent += 1;
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 1;
+    } else {
+        display.textContent += 1;
+    }
     value = parseInt(display.textContent);
 }
 
 function displayTwo() {
-    display.textContent += 2;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 2;
+    } else {
+        display.textContent += 2;
+    }    value = parseInt(display.textContent);
 }
 
 function displayThree() {
-    display.textContent += 3;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 3;
+    } else {
+        display.textContent += 3;
+    }    value = parseInt(display.textContent);
 }
 
 function displayFour() {
-    display.textContent += 4;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 4;
+    } else {
+        display.textContent += 4;
+    }    value = parseInt(display.textContent);
 }
 
 function displayFive() {
-    display.textContent += 5;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 5;
+    } else {
+        display.textContent += 5;
+    }    value = parseInt(display.textContent);
 }
 
 function displaySix() {
-    display.textContent += 6;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 6;
+    } else {
+        display.textContent += 6;
+    }    value = parseInt(display.textContent);
 }
 
 function displaySeven() {
-    display.textContent += 7;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 7;
+    } else {
+        display.textContent += 7;
+    }    value = parseInt(display.textContent);
 }
 
 function displayEight() {
-    display.textContent += 8;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 8;
+    } else {
+        display.textContent += 8;
+    }    value = parseInt(display.textContent);
 }
 
 function displayNine() {
-    display.textContent += 9;
-    value = parseInt(display.textContent);
+    if (value === 0) {
+        display.textContent = "";
+    }
+    if (display.textContent === "0") {
+        display.textContent = 9;
+    } else {
+        display.textContent += 9;
+    }    value = parseInt(display.textContent);
 }
 
 function addOperator() {
-    display.textContent = "";
-    operator = "addNumbers";
-    firstNumber = value;
+    if (firstNumber) {
+        secondNumber = value;
+    } else {
+        firstNumber = value;
+    }
+    if (secondNumber && operator) operate(firstNumber, secondNumber, operator);
+    operator = addNumbers;
     value = 0;
 }
 
 function subtractOperator() {
-    display.textContent = "";
-    operator = "subtractNumbers";
-    firstNumber = value;
+    if (firstNumber) {
+        secondNumber = value;
+    } else {
+        firstNumber = value;
+    }
+    if (secondNumber && operator) operate(firstNumber, secondNumber, operator);
+    operator = subtractNumbers;
     value = 0;
 }
 
 function multiplyOperator() {
-    display.textContent = "";
-    operator = "multiplyNumbers";
-    firstNumber = value;
+    if (firstNumber) {
+        secondNumber = value;
+    } else {
+        firstNumber = value;
+    }
+    if (secondNumber && operator) operate(firstNumber, secondNumber, operator);
+    operator = multiplyNumbers;
     value = 0;
 }
 
 function divideOperator() {
-    display.textContent = "";
-    operator = "divideNumbers";
-    firstNumber = value;
+    if (firstNumber) {
+        secondNumber = value;
+    } else {
+        firstNumber = value;
+    }
+    if (secondNumber && operator) operate(firstNumber, secondNumber, operator);
+    operator = divideNumbers;
     value = 0;
 }
 
 function displayClear() {
     display.textContent = "";
-    value = 0;
+    value = null;
+    firstNumber = value;
+    secondNumber = value;
+    operator = null;
 }
 
 zero.addEventListener("click", displayZero);
